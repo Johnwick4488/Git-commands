ecca0b4d1532c3da69f694656f94d9ac61dc6620
EC2 Default User <ec2-user@ip-172-0-10-204.ec2.internal>
2023-12-06 11:29:16 +0000

    vpc file added

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Devil-Subnets"
+  cidr = "15.0.0.0/21"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["15.0.0.0/24", "15.0.1.0/24"]
+  public_subnets  = ["15.0.2.0/23", "15.0.4.0/23"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
