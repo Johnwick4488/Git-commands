c56335bfb683e2a83d6c9d596e883fcb2d05830e
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2023-12-06 16:50:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "Githubactions-key"
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
