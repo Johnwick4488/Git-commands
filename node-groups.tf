3f9f43e4469b0b90321a8d2ab4216cd9f46c284f
EC2 Default User <ec2-user@ip-172-0-10-204.ec2.internal>
2023-12-13 07:00:33 +0000

    updated node3

--- node-groups.tf
+++ node-groups.tf
@@ -64,3 +64,45 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOn
   role       = aws_iam_role.node.name
 }
 
+
+# EKS Node Security Group
+resource "aws_security_group" "eks_nodes" {
+  name        = "${var.eksproject}-node-sg"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name                                           = "${var.eksproject}-node-sg"
+    "kubernetes.io/cluster/${var.eksproject}-cluster" = "owned"
+  }
+}
+
+# resource "aws_security_group_rule" "nodes_internal" {
+#   description              = "Allow nodes to communicate with each other"
+#   from_port                = 0
+#   protocol                 = "-1"
+#   security_group_id        = aws_security_group.eks_nodes.id
+#   source_security_group_id = aws_security_group.eks_nodes.id
+#   to_port                  = 65535
+#   type                     = "ingress"
+# }
+
+# resource "aws_security_group_rule" "nodes_cluster_inbound" {
+#   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+#   from_port                = 1025
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.eks_nodes.id
+#   source_security_group_id = aws_security_group.eks_cluster.id
+#   to_port                  = 65535
+#   type                     = "ingress"
+# }
+
+
+
