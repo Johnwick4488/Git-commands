41f9f7bebd851fce17e0731045d469fccb8f1d18
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2023-12-17 17:52:12 +0530

    Update node-groups.tf

--- node-groups.tf
+++ node-groups.tf
@@ -83,26 +83,3 @@ resource "aws_security_group" "eks_nodes" {
     "kubernetes.io/cluster/${var.eksproject}-cluster" = "owned"
   }
 }
-
-# resource "aws_security_group_rule" "nodes_internal" {
-#   description              = "Allow nodes to communicate with each other"
-#   from_port                = 0
-#   protocol                 = "-1"
-#   security_group_id        = aws_security_group.eks_nodes.id
-#   source_security_group_id = aws_security_group.eks_nodes.id
-#   to_port                  = 65535
-#   type                     = "ingress"
-# }
-
-# resource "aws_security_group_rule" "nodes_cluster_inbound" {
-#   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
-#   from_port                = 1025
-#   protocol                 = "tcp"
-#   security_group_id        = aws_security_group.eks_nodes.id
-#   source_security_group_id = aws_security_group.eks_cluster.id
-#   to_port                  = 65535
-#   type                     = "ingress"
-# }
-
-
-
