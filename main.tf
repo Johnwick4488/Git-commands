62224a74ccb743c5d7f4621d278e0adbcae841b0
EC2 Default User <ec2-user@ip-172-0-10-204.ec2.internal>
2023-12-29 14:00:44 +0000

    providers removed in main

--- main.tf
+++ main.tf
@@ -13,10 +13,6 @@ data "aws_ami" "ubuntu" {
 
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
